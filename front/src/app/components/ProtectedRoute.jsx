1cfe33a510efcbf711aa21f189b750733696cf30
LucasZibaitis <zibaitislucas@gmail.com>
2024-04-11 17:08:23 -0300

    protected route fixed

--- front/src/app/components/ProtectedRoute.jsx
+++ front/src/app/components/ProtectedRoute.jsx
@@ -6,13 +6,15 @@ export default function ProtectedRoute({ children }) {
   const router = useRouter();
 
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (!accessToken) {
-      router.replace("/");
+    if (typeof window !== "undefined") {
+      const accessToken = localStorage.getItem("accessToken");
+      if (!accessToken) {
+        router.replace("/");
+      }
     }
   }, [router]);
 
-  if (!localStorage.getItem("accessToken")) {
+  if (typeof window === "undefined" || !localStorage.getItem("accessToken")) {
     return null;
   }
 
